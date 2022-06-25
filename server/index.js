1eece14a408be93454d952cf339fd4f23cfe3343
Jubayer Al Alam <jubayer.alam@bjitgroup.com>
2022-06-25 23:06:28 +0600

    cors isshue

--- server/index.js
+++ server/index.js
@@ -24,6 +24,12 @@ app.use(cors(corsOptions));
 app.use(isAuth);
 app.use(roleGurd);
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(
   "/graphql",
   graphqlHTTP({
