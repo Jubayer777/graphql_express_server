9e7e2949b0e370256d38223bb7b8639b2f59ad9a
Jubayer Al Alam <jubayer.alam@bjitgroup.com>
2022-06-25 23:30:12 +0600

    cors isshue2

--- server/index.js
+++ server/index.js
@@ -5,11 +5,6 @@ require("dotenv").config();
 const port = process.env.PORT || 4000;
 const mongoose = require("mongoose");
 const cors = require("cors");
-const corsOptions ={
-  origin:'*', 
-  credentials:true,            //access-control-allow-credentials:true
-  optionSuccessStatus:200,
-}
 
 const graphQlSchema = require("./graphql/schema/index");
 const graphQlResolvers = require("./graphql/resolvers/index");
@@ -19,16 +14,18 @@ const roleGurd = require("./middleware/roleGurd");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
 
 app.use(bodyParser.json());
-app.use(cors(corsOptions));
+// app.use(cors());
+
+app.use(
+  cors({
+    origin: ['https://stock-track-woad.vercel.app/', 'http://localhost:3000/'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT'],
+  })
+)
 
 app.use(isAuth);
 app.use(roleGurd);
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.use(
   "/graphql",
