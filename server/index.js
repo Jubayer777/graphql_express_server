3e0de253a2b75e64da7f98e0806e8bb8777d38e9
Jubayer Al Alam <jubayer.alam@bjitgroup.com>
2022-10-11 09:54:49 +0600

    cors changes3

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 4000;
 const mongoose = require("mongoose");
-// const cors = require("cors");
+const cors = require("cors");
 
 const graphQlSchema = require("./graphql/schema/index");
 const graphQlResolvers = require("./graphql/resolvers/index");
@@ -15,6 +15,13 @@ const graphqlHTTP = require("express-graphql").graphqlHTTP;
 
 app.use(bodyParser.json());
 // app.use(cors());
+app.use(
+  cors({
+    origin: ['https://graphql-client-cyan.vercel.app/', 'http://localhost:3000/'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT'],
+  })
+)
 
 app.use(isAuth);
 app.use(roleGurd);
