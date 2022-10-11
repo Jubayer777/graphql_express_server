debadf90c6f08500a31cb7dbdf4b12a28a5c9a4b
Jubayer Al Alam <jubayer.alam@bjitgroup.com>
2022-10-11 10:55:04 +0600

    cors changes53

--- server/index.js
+++ server/index.js
@@ -14,12 +14,13 @@ const roleGurd = require("./middleware/roleGurd");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
 
 app.use(bodyParser.json());
-app.use(cors());
-// const corsOptions={
-//     origin: ['https://graphql-client-cyan.vercel.app/', 'http://localhost:3000/'],
-//     credentials: true,
-//   }
-// server.applyMiddleware({ app, cors: corsOptions });
+
+const corsOptions={
+    origin: ['https://graphql-client-cyan.vercel.app/', 'http://localhost:3000/'],
+    credentials: true,
+  }
+  app.use(cors(corsOptions));
+
 
 app.use(isAuth);
 app.use(roleGurd);
@@ -42,6 +43,11 @@ mongoose
     console.log("connection error", err);
   });
 
+  server.applyMiddleware({
+    app,
+    path: '/',
+    cors: false, // disables the apollo-server-express cors to allow the cors middleware use
+  })
 app.listen(port||4000, () => {
   console.log(`This site is running on http://localhost:${port}`);
 });
